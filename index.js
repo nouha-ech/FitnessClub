6c1951d51c2997bd0397de2767d0874879807d48
nouhaila echchnaoui <nouhaila.chan2001@gmail.com>
2024-09-06 08:40:44 +0100

    com

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import mysql from "mysql2/promise";
-import dotenv from "dotenv";
+import mysql from "mysql2/promise";  // pour mysql
+import dotenv from "dotenv";    // pour env var
 
 dotenv.config();
 
