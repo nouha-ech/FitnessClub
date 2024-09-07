f0ce50072dac6e842161e8673a995f1d34da22a2
nouhaila echchnaoui <nouhaila.chan2001@gmail.com>
2024-09-07 08:07:19 +0100

    login sccript

---
+++ public/script.js
@@ -0,0 +1,20 @@
+
+// login func
+function login(){
+    const username = document.getElementById("login-username").value;
+    const password = document.getElementById("login-password").value;
+
+   fetch('/Login',{
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json'},
+    body: JSON.stringify({
+        username,
+        password
+    })
+   })
+}
+.then (res => res.json())
+.then (data =>{
+    // CONTINUE THISSS
+    // if data token 
+})
